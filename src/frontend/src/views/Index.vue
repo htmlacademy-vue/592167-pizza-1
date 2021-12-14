4c31877e85670d5db05661bfdb46a1f7677cc104
Agafon <dragafony@gmail.com>
2021-12-14 22:36:36 +0400

    Добавляет компонент Vue Index.vue.

---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header />
+    <Main />
+  </div>
+</template>
+
+<script>
+import misc from "../static/misc.json";
+import pizza from "../static/pizza.json";
+import user from "../static/user.json";
+
+import Header from "./Header";
+import Main from "./Main";
+
+export default {
+  name: "Index",
+  data() {
+    return { misc, pizza, user };
+  },
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
+
+<!--<style scoped>-->
+
+<!--</style>-->
