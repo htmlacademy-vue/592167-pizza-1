8e5edca7f388c85fb2ad6bceefbcf6cda28537be
Agafon <dragafony@gmail.com>
2022-03-02 10:55:22 +0300

    Создает Vuex store и подключает его в main.js.

---
+++ src/frontend/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import misc from "@/static/misc.json";
+import user from "@/static/user.json";
+
+Vue.use(Vuex);
+
+const state = () => ({
+  misc,
+  user,
+  doughSize: "light",
+  sauceInfo: "tomato",
+  pizzaDiameter: "small",
+  pizzaName: "",
+  sum: 0,
+  ingredients: [],
+  selectedIngredients: {},
+});
+
+export default new Vuex.Store({
+  state,
+});
