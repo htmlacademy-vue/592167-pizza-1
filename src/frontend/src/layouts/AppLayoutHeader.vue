44ee68f223d1f78af8a7c9ad9c0f9633360c79d9
Agafon <dragafony@gmail.com>
2022-03-04 13:50:49 +0300

    Меняет путь, при нажатии кнопки выход, делает переход на главную страницу.

--- src/frontend/src/layouts/AppLayoutHeader.vue
+++ src/frontend/src/layouts/AppLayoutHeader.vue
@@ -58,7 +58,7 @@ export default {
     ...mapActions("Auth", ["logout"]),
     logoutUser() {
       this.logout();
-      this.$router.push("/login");
+      this.$router.push("/");
     },
   },
 };
