291f77ea132d5d65020dcfdbef8213b8364d347f
Agafon <dragafony@gmail.com>
2022-03-14 09:27:25 +0300

    Добавляет в проект логику работы с адресом пользователя.

--- src/frontend/src/modules/cart/CartMain.vue
+++ src/frontend/src/modules/cart/CartMain.vue
@@ -9,49 +9,7 @@
 
         <cart-additional />
 
-        <div class="cart__form">
-          <div class="cart-form">
-            <label class="cart-form__select">
-              <span class="cart-form__label">Получение заказа:</span>
-
-              <select name="test" class="select">
-                <option value="1">Заберу сам</option>
-                <option value="2">Новый адрес</option>
-                <option value="3">Дом</option>
-              </select>
-            </label>
-
-            <label class="input input--big-label">
-              <span>Контактный телефон:</span>
-              <input type="text" name="tel" placeholder="+7 999-999-99-99" />
-            </label>
-
-            <div class="cart-form__address">
-              <span class="cart-form__label">Новый адрес:</span>
-
-              <div class="cart-form__input">
-                <label class="input">
-                  <span>Улица*</span>
-                  <input type="text" name="street" />
-                </label>
-              </div>
-
-              <div class="cart-form__input cart-form__input--small">
-                <label class="input">
-                  <span>Дом*</span>
-                  <input type="text" name="house" />
-                </label>
-              </div>
-
-              <div class="cart-form__input cart-form__input--small">
-                <label class="input">
-                  <span>Квартира</span>
-                  <input type="text" name="apartment" />
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
+        <cart-form />
       </template>
       <template v-else>
         <div class="sheet cart__empty">
@@ -65,11 +23,12 @@
 <script>
 import CartList from "@/modules/cart/components/CartList";
 import CartAdditional from "@/modules/cart/components/CartAdditional";
+import CartForm from "@/modules/cart/components/CartForm";
 import { mapGetters } from "vuex";
 
 export default {
   name: "CartMain",
-  components: { CartList, CartAdditional },
+  components: { CartList, CartAdditional, CartForm },
   computed: {
     ...mapGetters("Cart", ["pizzas"]),
   },
