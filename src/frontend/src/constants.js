e7a5c073a890763e26833062c09d9e4adc24a501
Agafon <dragafony@gmail.com>
2022-04-15 09:18:11 +0300

    Убирает неиспользуемые методы и константы.

--- src/frontend/src/constants.js
+++ src/frontend/src/constants.js
@@ -1,35 +1,12 @@
-export const MOVE = "move";
 export const DATA_TRANSFER_PAYLOAD = "payload";
 export const MAX_INGREDIENT_COUNT = 3;
+export const SEC = 1000;
+export const MESSAGE_LIVE_TIME = 3 * SEC;
 export const MIN_INGREDIENT_COUNT = 0;
-
-export const SIZE_MULTIPLIER = {
-  small: 1,
-  normal: 2,
-  big: 3,
-};
-
-export const DOUGH_PRICE = {
-  light: 300,
-  large: 300,
-};
-
-export const SAUCES_PRICE = {
-  tomato: 50,
-  creamy: 50,
-};
-
-export const PIZZA_SIZE = {
-  1: "small",
-  2: "normal",
-  3: "big",
-};
+export const MOVE = "move";
+export const MY_FIRST_ADDRESS = 3;
+export const NEW_ADDRESS = 2;
+export const PICK_UP_MYSELF = 1;
 
 // eslint-disable-next-line
 export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-export const SEC = 1000;
-export const MESSAGE_LIVE_TIME = 3 * SEC;
-export const PICK_UP_MYSELF = 1;
-export const NEW_ADDRESS = 2;
-export const MY_FIRST_ADDRESS = 3;
