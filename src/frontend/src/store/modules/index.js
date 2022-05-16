cfa0a3a137252df5ce58205206d3db283c9169c7
Agafon <dragafony@gmail.com>
2022-05-16 15:18:51 +0300

    Добавляет автоматическое подключение модулей vuex.

--- src/frontend/src/store/modules/index.js
+++ src/frontend/src/store/modules/index.js
@@ -1,13 +1,10 @@
-import Auth from "@/store/modules/auth.store";
-import Builder from "@/store/modules/builder.store";
-import Cart from "@/store/modules/cart.store";
-import Profile from "@/store/modules/profile.store";
-import Orders from "@/store/modules/orders.store";
+const requireContext = require.context("@/modules/", true, /store\.js$/);
 
-export default {
-  Auth,
-  Builder,
-  Cart,
-  Profile,
-  Orders,
-};
+export default requireContext.keys().reduce((modules, filename) => {
+  const moduleName = filename
+    .split("/")[1]
+    .replace(/^\w/, (c) => c.toUpperCase());
+  modules[moduleName] =
+    requireContext(filename).default || requireContext(filename);
+  return modules;
+}, {});
